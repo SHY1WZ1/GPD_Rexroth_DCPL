a1546fa858aeff8d19be82718658f241eab294af
shy1wz1 <Andrii.Shyrokov@boschrexroth.pl>
2022-01-11 21:27:56 +0100

    delete bonus_package column from gpd_person

--- gpd/admin.py
+++ gpd/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Person, TableAdmin
+from .models import Person, TableAdmin, BonusAdmin
 from django.utils.translation import ugettext_lazy
 # Register your models here.
 
 
 admin.site.register(Person,TableAdmin)
+#admin.site.register(BonusPackage,BonusAdmin)
 admin.site.site_header = 'Bosch Rexroth administration'
 admin.site.index_title = 'GPD administration'
--- gpd/models.py
+++ gpd/models.py
@@ -2,19 +2,23 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
+
+
+
+
 class Person(models.Model):
     CATEGORY = (
 			('Active', 'Active'),
 			('Deactivated', 'Deactivated'),
 			) 
-    PACKAGE = (
-			('1', '1'),
-			('2', '2'),
-            ('1/2', '1/2'),
-            ('K2', 'K2'),
-            ('3', '3'),
-            ('K3', 'K3'),
-			) 
+   # PACKAGE = (
+			#('1', '1'),
+			#('2', '2'),
+   #         ('1/2', '1/2'),
+   #         ('K2', 'K2'),
+   #         ('3', '3'),
+   #         ('K3', 'K3'),
+			#) 
     name = models.CharField(max_length=30,default='')
     lastname = models.CharField(max_length=30, default='')
     department = models.CharField(max_length=30, default='')
@@ -23,14 +27,23 @@ class Person(models.Model):
     position = models.CharField(max_length=30, default='')
     email = models.CharField(max_length=50, default='')
     status = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    bonus_package = models.CharField(max_length=200, null=True, choices=PACKAGE)
+    
+    
 
     def __str__(self):
         return self.name +' '+self.lastname+' '+ self.end_user
 
 
 
+    
+
 class TableAdmin(admin.ModelAdmin):
-    list_display= ('name','lastname', 'end_user','mng_end_user','position','email', 'status', 'bonus_package')
+    list_display= ('name','lastname', 'end_user','mng_end_user','position','email', 'status')
+
+
 class Meta:
         verbose_name="Bosch Rexroth GPD Assessment"
+
+class BonusAdmin(admin.ModelAdmin):
+    list_display= ('bonus_group_name','individual_comp_weight')
+
